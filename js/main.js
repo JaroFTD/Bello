7501ba55e0342b1b1a42e070f5dd057643aebfc9
YaloFTD <119479151+YaloFTD@users.noreply.github.com>
2023-10-03 09:19:27 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,298 @@
+"use strict";
+// МЕНЮ БУРГЕР
+let menu = document.querySelector('.icon-menu');
+let menuBody = document.querySelector('.menu__body');
+menu.addEventListener('click', function () {
+   document.body.classList.toggle('_lock');
+   menu.classList.toggle('_active');
+   menuBody.classList.toggle('_active');
+});
+
+// ЛИПКИЙ HEADER
+let header = document.querySelector('.header');
+
+document.onscroll = function () {
+   let scroll = window.scrollY;
+   if (scroll > 0){
+      header.classList.add('_fixed');
+   } else {
+      header.classList.remove('_fixed');
+   }
+}
+// ВАЛИДАЦИЯ ФОРМЫ
+let forms = document.querySelectorAll('form');
+if (forms.length > 0) { 
+   intitForms(forms);
+}
+function intitForms(forms) {
+   for (let i = 0; i < forms.length; i++){
+      initForm(forms[i]);
+   }
+
+   function initForm(form) { 
+      form.addEventListener('submit', formSend);
+
+      let resultMessage = document.createElement('div');
+      resultMessage.classList.add('_goodmessage');
+      form.appendChild(resultMessage);
+
+      async function formSend(e) {
+         e.preventDefault();
+
+         let error = formValidate(form);
+
+         // для отправки спомощью AJAX
+         const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : '#';
+         const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : 'GET';
+         const formData = new FormData(form);
+         
+         if (error === 0) {
+            form.classList.add('_sending');
+
+            // для отправки спомощью AJAX
+				const response = await fetch(formAction, {
+					method: formMethod,
+					body: formData
+            });
+            
+            if (response.ok) {
+               let result = await response.json();
+               alert(result.message);
+               form.classList.remove('_sending');
+               form.reset();
+               resultMessage.classList.remove('_errorMessage');
+               resultMessage.textContent = 'Отправленно'; 
+            }else{
+               alert('Ошибка');
+               form.classList.remove('_sending');
+            }
+         } else {
+            resultMessage.classList.add('_errorMessage');
+            resultMessage.textContent = 'Ошибка';
+         }
+      }
+
+      function formValidate(form) { 
+         let error = 0;
+         let formReq = form.querySelectorAll('._req');
+
+         for (let i = 0; i < formReq.length; i++){
+            const input = formReq[i];
+            formRemoveError(input);
+            // проверяем input на email
+            if (input.classList.contains('_email')) {
+               if (emailTest(input)) {
+                  formAddError(input);
+                  error++;
+               }
+            // проверяем input на checkbox
+            } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+               formAddError(input);
+               error++;
+            } else {
+               // проверяем input на пустые поля
+               if (input.value === '') {
+                  formAddError(input);
+                  error++;
+               }
+            }
+         }
+         return error;
+      }
+      // Функция для добавления класса error
+      function formAddError(input) { 
+         input.parentElement.classList.add('_error');
+         input.classList.add('_error');
+      }
+      // Функция для удаления класса error
+      function formRemoveError(input) { 
+         input.parentElement.classList.remove('_error');
+         input.classList.remove('_error');
+      }
+      // Функия теста email
+      function emailTest(input) {
+         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+      }
+   }
+}
+/**
+ * @typedef {Object} dNode
+ * @property {HTMLElement} parent
+ * @property {HTMLElement} element
+ * @property {HTMLElement} to
+ * @property {string} breakpoint
+ * @property {string} order
+ * @property {number} index
+ */
+/**
+ * @typedef {Object} dMediaQuery
+ * @property {string} query
+ * @property {number} breakpoint
+ */
+/**
+ * @param {'min' | 'max'} type
+ */
+function useDynamicAdapt(type = 'max') {
+   const className = '_dynamic_adapt_'
+   const attrName = 'data-da'
+
+   /** @type {dNode[]} */
+   const dNodes = getDNodes()
+
+   /** @type {dMediaQuery[]} */
+   const dMediaQueries = getDMediaQueries(dNodes)
+
+   dMediaQueries.forEach((dMediaQuery) => {
+      const matchMedia = window.matchMedia(dMediaQuery.query)
+      // массив объектов с подходящим брейкпоинтом
+      const filteredDNodes = dNodes.filter(({ breakpoint }) => breakpoint === dMediaQuery.breakpoint)
+      const mediaHandler = getMediaHandler(matchMedia, filteredDNodes)
+      matchMedia.addEventListener('change', mediaHandler)
+
+      mediaHandler()
+   })
+
+   function getDNodes() {
+      const result = []
+      const elements = [...document.querySelectorAll(`[${attrName}]`)]
+
+      elements.forEach((element) => {
+         const attr = element.getAttribute(attrName)
+         const [toSelector, breakpoint, order] = attr.split(',').map((val) => val.trim())
+
+         const to = document.querySelector(toSelector)
+
+         if (to) {
+            result.push({
+               parent: element.parentElement,
+               element,
+               to,
+               breakpoint: breakpoint ?? '767',
+               order: order !== undefined ? (isNumber(order) ? Number(order) : order) : 'last',
+               index: -1,
+            })
+         }
+      })
+
+      return sortDNodes(result)
+   }
+
+   /**
+    * @param {dNode} items
+    * @returns {dMediaQuery[]}
+    */
+   function getDMediaQueries(items) {
+      const uniqItems = [...new Set(items.map(({ breakpoint }) => `(${type}-width: ${breakpoint}px),${breakpoint}`))]
+
+      return uniqItems.map((item) => {
+         const [query, breakpoint] = item.split(',')
+
+         return { query, breakpoint }
+      })
+   }
+
+   /**
+    * @param {MediaQueryList} matchMedia
+    * @param {dNodes} items
+    */
+   function getMediaHandler(matchMedia, items) {
+      return function mediaHandler() {
+         if (matchMedia.matches) {
+         items.forEach((item) => {
+            moveTo(item)
+         })
+
+         items.reverse()
+         } else {
+         items.forEach((item) => {
+            if (item.element.classList.contains(className)) {
+               moveBack(item)
+            }
+         })
+
+         items.reverse()
+         }
+      }
+   }
+
+   /**
+    * @param {dNode} dNode
+    */
+   function moveTo(dNode) {
+      const { to, element, order } = dNode
+      dNode.index = getIndexInParent(dNode.element, dNode.element.parentElement)
+      element.classList.add(className)
+
+      if (order === 'last' || order >= to.children.length) {
+         to.append(element)
+
+         return
+      }
+
+      if (order === 'first') {
+         to.prepend(element)
+
+         return
+      }
+
+      to.children[order].before(element)
+   }
+
+   /**
+    * @param {dNode} dNode
+    */
+   function moveBack(dNode) {
+      const { parent, element, index } = dNode
+      element.classList.remove(className)
+
+      if (index >= 0 && parent.children[index]) {
+         parent.children[index].before(element)
+      } else {
+         parent.append(element)
+      }
+   }
+
+   /**
+    * @param {HTMLElement} element
+    * @param {HTMLElement} parent
+    */
+   function getIndexInParent(element, parent) {
+      return [...parent.children].indexOf(element)
+   }
+
+   /**
+    * Функция сортировки массива по breakpoint и order
+    * по возрастанию для type = min
+    * по убыванию для type = max
+    *
+    * @param {dNode[]} items
+    */
+   function sortDNodes(items) {
+      const isMin = type === 'min' ? 1 : 0
+
+      return [...items].sort((a, b) => {
+         if (a.breakpoint === b.breakpoint) {
+         if (a.order === b.order) {
+            return 0
+         }
+
+         if (a.order === 'first' || b.order === 'last') {
+           return -1 * isMin
+         }
+
+         if (a.order === 'last' || b.order === 'first') {
+           return 1 * isMin
+         }
+
+         return 0
+      }
+
+       return (a.breakpoint - b.breakpoint) * isMin
+      })
+   }
+
+   function isNumber(value) {
+      return !isNaN(value)
+   }
+}
+useDynamicAdapt();
